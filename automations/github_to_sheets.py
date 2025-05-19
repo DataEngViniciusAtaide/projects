c3ac8b8188f0d3dfa4d2a2d4165f2757f599e251
Vinicius Ataide <dataeng.viniciusataide@gmail.com>
2025-05-18 21:05:43 -0300

    Update github_to_sheets.py

--- automations/github_to_sheets.py
+++ automations/github_to_sheets.py
@@ -26,7 +26,7 @@ PROJECT_ID = "PVT_kwHOCqE-wc4ArbN8"
 SPREADSHEET_ID = "1vdwjCK2vrPm0L4-sUWcmwFYwz3DaYRzA2PYN-t9kWsQ"
 SHEET_NAME = "Sheet1"
 GITHUB_TOKEN = os.environ.get("MY_GITHUB_TOKEN")
-CREDENTIALS_FILE = "automations/credentials.json"
+CREDENTIALS_FILE = "credentials.json"
 
 # Definindo os cabeçalhos esperados
 EXPECTED_HEADERS = ["Número", "Título", "Estado", "URL", "Status", "Prioridade", "Complexidade", "Departamento", "Data Atualização"]
